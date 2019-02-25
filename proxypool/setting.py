2c774173c1ec58cef0d716f19096a591b03165dd
lalalalalalalala123 <42734468+lalalalalalalala123@users.noreply.github.com>
2019-02-25 17:49:45 +0800

    Update setting.py

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -1,11 +1,11 @@
 # Redis数据库地址
-REDIS_HOST = '47.106.153.117'
+REDIS_HOST = 'localhost'
 
 # Redis端口
 REDIS_PORT = 6379
 
 # Redis密码，如无填None
-REDIS_PASSWORD = 'huweidong'
+REDIS_PASSWORD = None
 
 REDIS_KEY = 'proxies'
 
